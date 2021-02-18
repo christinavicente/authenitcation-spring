ff0db1c834cdd50fed3faf47b40b1009bed10f84
Christina Vicente <christina.vicente@hcl.com>
2021-02-18 10:14:19 -0500

    adjusted views, added model and controller

---
+++ src/main/java/com/example/authenticationspring/LoginController.java
@@ -0,0 +1,4 @@
+package com.example.authenticationspring;
+
+public class LoginController {
+}
---
+++ src/main/java/com/example/authenticationspring/UserRepo.java
@@ -0,0 +1,6 @@
+package com.example.authenticationspring;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo extends CrudRepository<User, Integer> {
+}
