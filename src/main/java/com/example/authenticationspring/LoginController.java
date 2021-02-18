8a7a911c89756da65c32f518eca332d4dea89fee
Christina Vicente <christina.vicente@hcl.com>
2021-02-18 17:28:59 -0500

    completed controller, added jsp

--- src/main/java/com/example/authenticationspring/LoginController.java
+++ src/main/java/com/example/authenticationspring/LoginController.java
@@ -1,14 +1,39 @@
 package com.example.authenticationspring;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    UserRepo userRepo;
 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginUser(@RequestParam String username, @RequestParam String password){
+        if(authenticate(username,password)){
+            return "hello";
+        }else{
+            return "incorrect";
+        }
 
+    }
 
+    private boolean authenticate(String username, String password){
+        User userName = userRepo.FindByUsername(username);
+        User userPass = userRepo.FindByPassword(password);
+        boolean result=false;
+
+        if((userName!=null)&&(userPass!=null)&&(userName.getId()==userPass.getId())){
+            result=true;
+        }
+
+        return result;
+    }
 
     public void savedUsers() {
         User user1 = new User();
--- src/main/java/com/example/authenticationspring/UserRepo.java
+++ src/main/java/com/example/authenticationspring/UserRepo.java
@@ -3,7 +3,7 @@ package com.example.authenticationspring;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<User, Integer> {
-        User FindByUsername();
-        User FindByPassword();
+        User FindByUsername(String username);
+        User FindByPassword(String password);
 
 }
